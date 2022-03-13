f5d8da8c517746de89d7b320ed3dbb37cba0ffaf
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2022-03-13 14:41:21 +0700

    install firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import {} from './plugins/firebase'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
